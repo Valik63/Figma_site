dc168fe0eeb1ff41e0af198cfe3ed843f63d36a2
Valik63 <78017490+Valik63@users.noreply.github.com>
2021-05-25 17:00:13 +0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+function openH() {
+    document.location.href = "index.html";
+}
+function openR() {
+    document.location.href = "reservations.html";
+}
+function openC() {
+    document.location.href = "contacts.html";
+}
+function openA() {
+    document.location.href = "about.html";
+}
